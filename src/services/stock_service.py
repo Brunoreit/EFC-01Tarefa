9bb8def3657e642a070440b76f41b10736f7c033
PXC-PUC <pedro.xc@puccampinas.edu.br>
2026-05-21 10:35:23 -0300

    Adding stock_service

---
+++ src/services/stock_service.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class StockService:
+    """Responsabilidade única: validação de estoque (SRP)."""
+
+    # TODO: integrar com sistema de estoque externo
+    _stock: dict = {
+        "produto1": 100,
+        "produto2": 50,
+        "produto3": 75,
+    }
+
+    def validate(self, itens_raw: List[dict]) -> bool:
+        for item in itens_raw:
+            if item["nome"] not in self._stock:
+                print(f"Produto {item['nome']} nao encontrado!")
+                return False
+            if self._stock[item["nome"]] < item["q"]:
+                print(f"Estoque insuficiente para {item['nome']}!")
+                return False
+        return True
